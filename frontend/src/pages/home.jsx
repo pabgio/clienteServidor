99476e76517194d831bc6479822a9e6ad29824aa
pabgio <pabsan@alunos.utfpr.edu.br>
2023-05-18 00:07:54 -0300

    Ajustes

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { IconContext } from "react-icons";
+import { FaUser } from "react-icons/fa";
 
 export default function HomePage() {
   const router = useRouter();
@@ -21,18 +23,33 @@ export default function HomePage() {
     <div>
       <header className="bg-gray-800 py-4">
         <div className="container mx-auto flex justify-between items-center px-4">
-          <h1 className="text-white text-xl font-bold">Minha Aplicação</h1>
-          <div>
-            <Link href="/perfil " className="text-white hover:text-gray-300 mx-4">
-              Perfil
-            </Link>
-            <button onClick={handleLogout} className="text-white hover:text-gray-300">
-              Logout
-            </button>
-          </div>
+          <h1 className="text-white text-2xl font-bold">Minha Aplicação</h1>
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="/perfil" className="text-white hover:text-gray-300 flex items-center">
+                    <IconContext.Provider value={{ className: "mr-1" }}>
+                      <FaUser />
+                    </IconContext.Provider>
+                    Perfil
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={handleLogout}
+                  className="text-white hover:text-gray-300"
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
-      <h1>Bem-vindo à página inicial!</h1>
+      <div className="container mx-auto py-8">
+        <h1 className="text-3xl font-bold mb-4">Bem-vindo à página inicial!</h1>
+        <p className="text-gray-600">Esta é a página principal da aplicação.</p>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/perfil.jsx
+++ frontend/src/pages/perfil.jsx
@@ -1,15 +1,55 @@
 import React from "react";
+import Link from "next/link";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import Input from "@/components/form/input";
+import Button from "@/components/form/button";
+import { useLogin } from "@/hooks/useLogin";
+import Error from "@/components/error";
 
 export default function ProfilePage() {
+    const [email, getEmail] = useState("");
+    const [nome, setNome] = useState("");
+    const { login, error, isLoading } = useLogin();
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        await login(email, senha);
+    }
     
     return (
-        <div>
-            <h1>Perfil</h1>
+        <div className="bg-gray-800 min-h-screen flex items-center justify-center">
+          <div className="card bg-white shadow-lg max-w-md md:w-[100%] w-full max-h-[90vh]">
+            <div className="relative h-72">
+              <Image src="/road2.jpg" alt="rua" width={500} height={500} priority />
+            </div>
+    
+            <div className="p-8">
+              <h3 className="text-3xl md:text-4xl font-bold mb-4 text-center text-gray-800">
+                Perfil
+              </h3>
+    
+              <div className="mb-6">
+                <label className="block text-sm font-medium text-gray-700">Nome:</label>
+                <span className="text-gray-900">{nome}</span>
+              </div>
+    
+              <div className="mb-6">
+                <label className="block text-sm font-medium text-gray-700">Email:</label>
+                <span className="text-gray-900">{email}</span>
+              </div>
+    
+              <div className="flex justify-center">
+                <Link href="/alterar-usuario" className="text-blue-500 hover:text-blue-700">
+                  Alterar Usuário
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-
-       
-     
-     
-    );
+      );
   }
   
